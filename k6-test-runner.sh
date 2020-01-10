299047d121ef4a0c02c2e07397f420f11487b238
aonamrata <aonamrata@gmail.com>
2020-01-10 17:40:18 +0530

    Add some more test runner

--- k6-test-runner.sh
+++ k6-test-runner.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 docker pull loadimpact/k6
+docker run --network="host" -i loadimpact/k6 run --vus 100 -i 1000 - <k6-script.js
+sleep 5
+
+docker run --network="host" -i loadimpact/k6 run --vus 500 -i 1000 - <k6-script.js
+sleep 5
+
 docker run --network="host" -i loadimpact/k6 run --vus 10 -i 1000 - <k6-script.js
+sleep 5
